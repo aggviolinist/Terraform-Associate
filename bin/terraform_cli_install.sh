79e0d21075717ff31c468e4f60a4441959a7ded5
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-06-18 21:35:12 +0000

    tf

--- bin/terraform_cli_install.sh
+++ bin/terraform_cli_install.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+cd /workspace
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl 
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | 
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform
+cd $PROJECT_ROOT
